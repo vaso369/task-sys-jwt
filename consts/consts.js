0d626e4f4e403ee17cc7491782d8d5e20ca8a971
vaso369 <59292985+vaso369@users.noreply.github.com>
2020-02-18 16:17:30 +0100

    updated

--- consts/consts.js
+++ consts/consts.js
@@ -2,11 +2,11 @@
 // import { useStateGlobal } from "../src/GlobalState";
 // const state = useStateGlobal();
 
-export const url = "https://api-task-management.000webhostapp.com/backend/index.php";
+export const url = "https://task-sys-backend.herokuapp.com/index.php";
 
 export const picturePath =
-  "https://api-task-management.000webhostapp.com/backend/app/";
+  "https://task-sys-backend.herokuapp.com/app/";
 
-export const urlRedirect = "https://task-sys-api.000webhostapp.com";
+export const urlRedirect = "https://task-sys-backend.herokuapp.com";
 
-export const defaultPicture = `https://api-task-management.000webhostapp.com/backend/app/assets/avatar-placeholder.png`;
+export const defaultPicture = `https://task-sys-backend.herokuapp.com/app/assets/avatar-placeholder.png`;
